a7e7d0f0f657f88ca72a78945dea98e9ab85eebb
EmanuelMarquessf <emanuel.marquessf@gmail.com>
2024-04-30 14:06:33 -0300

    ApiKey - .env configure

--- src/services/openWeatherAPI.js
+++ src/services/openWeatherAPI.js
@@ -1,5 +1,6 @@
+import { apiKey } from "../config/config";
 export async function fetchData(tipedCity, state){
-  const data = fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${tipedCity},${state},BR&limit=10&appid=03a697de3ffb3a5a79da8c382631b852`)
+  const data = fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${tipedCity},${state},BR&limit=10&appid=03a697de3ffb3a5a79da8c382631b852`)
   .then(response => response.json())
   console.log(state)
   return data;
