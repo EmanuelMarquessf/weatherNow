33e0bd61a4a8cddae63b544b862c461490fb63f3
EmanuelMarquessf <emanuel.marquessf@gmail.com>
2024-05-07 19:53:36 -0300

    Design implement

--- app.vue
+++ app.vue
@@ -27,17 +27,74 @@
 </script>
 
 <template>
-  <div>
-    <label for="">City name</label>
-    <input v-model="tipedCity" type="text">
-    <label for="">Estado</label>
-    <input v-model="tipedState" type="text">
-    <button @click="loadCountry()">pesquisar</button>
-    <p v-if="loadadedWeather">{{ Math.ceil(temperatureConvert( loadadedWeather.main.temp))}}°C</p>
-
-  </div>
+  <main>
+    <div class="formContainer flexRow">
+      <div id="cityInput"class="inputContainer flexCol">
+        <label for="">City name</label>
+        <input v-model="tipedCity" type="text">
+      </div>
+      <div id="stateInput" class="inputContainer flexCol">
+        <label for="">Estado</label>
+        <input v-model="tipedState" type="text">
+      </div>
+      <button @click="loadCountry()"><img src="/icons/search.svg" alt=""></button>
+    </div>
+    <span v-if="loadadedWeather">{{ Math.ceil(temperatureConvert( loadadedWeather.main.temp))}}°C</span>
+    <span v-else>Escolha sua localização!</span>
+  </main>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
+@import './src/styles/_colors';
 
+main{
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  margin: 4rem;
+  background-color: $windowColor;
+  font-family: sans-serif;
+  border-radius: 15px;
+  gap: 4rem;
+  span{
+    font-size: larger;
+    text-align: center
+  }
+  .formContainer{
+    gap: 1rem;
+    justify-content: space-between;
+    align-items: end;
+    .inputContainer{
+      gap:0.5rem;
+      label{
+        font-size: medium;
+      }
+      input{
+        padding: 0.5rem;
+        font-size: x-large;
+        color: #2a2a2a;
+      }
+    }
+    #cityInput{
+      width: 90%;
+    }
+    #stateInput{
+      width: 10%;
+    }
+    button{
+      padding: 0rem 0.5rem;
+      height: 50px;
+      font-size: medium;
+    }
+  }
+}
+
+.flexCol{
+  display: flex;
+  flex-direction: column;
+}
+.flexRow{
+  display: flex;
+  flex-direction: row;
+}
 </style>
